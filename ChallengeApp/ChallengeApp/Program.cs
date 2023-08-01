7dae9d0a33f408d29dfe0b063f3845152c692962
Wojciech Wawrzynkiewicz <wojciech.wawrzynkiewicz1337@gmail.com>
2023-08-01 20:35:00 +0200

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool women = true;
+int age = 23;
+
+if (women == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (women == false && age < 17)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nie jestem żadnym z powyższych wariantów, " +
+        "jestem: " + name + " i mam " + age + "lat/a");
+}
